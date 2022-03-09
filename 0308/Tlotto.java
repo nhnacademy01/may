a0feca5717d5e09a25d2182a747bcfb928c1d246
minayoung5 <71637466+minayoung5@users.noreply.github.com>
2022-03-10 00:23:59 +0900

    +Tlotto

--- 0308/Tlotto.java
+++ 0308/Tlotto.java
@@ -3,6 +3,12 @@ import java.util.Random;
 public class Tlotto {
     public static void main(String[] args) {
         Random rd = new Random();
-
+        int[] lotto = new int[6];
+        for (int i = 0; i < 6; i++) {
+            lotto[i] = rd.nextInt(45)+1;
+            for (int j = 0; j < i; j++)
+                {if (lotto[i]==lotto[j]) {i--;}}}
+        for (int i = 0; i < lotto.length; i++)
+            {System.out.println(lotto[i]);}
     }
 }
